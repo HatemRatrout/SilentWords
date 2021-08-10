224b7313d313002115393264eea9fd28306ffb61
yousef <ronorozoroz@hotmail.com>
2021-08-10 13:59:08 +0300

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const port = 8000;
 require("./server/config/mongoose.config");
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-const AllMyUserRoutes = require("./server/routes/word.routes");
-AllMyUserRoutes(app);
+require("./server/routes/word.routes")(app);
 
-app.listen(8000, () => console.log("The server is all fired up on port 8000"));
+
+app.listen(port, () => console.log("The server is all fired up on port 8000"));
